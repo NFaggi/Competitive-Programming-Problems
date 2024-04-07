ab954a650c04cf9beccdcc7955a0f34dec167683
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-04-07 02:14:20 -0300

    Create Josephus Problem II.cpp

---
+++ CSES/Sorting and Searching/Josephus Problem II.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<long long, long long>>v;
+vector<long long>vOr;
+vector<long long>I;
+vector<long long>D;
+
+long long SrcDel(long long p, long long nodo)
+{
+    long long der, izq, ant;
+    if (v[nodo].first == 0)
+    {
+        return 0;
+    }
+    if (p > D[nodo] || p < I[nodo])
+        return -1;
+    if (p == I[nodo] && p == D[nodo])
+    {
+        ant = v[nodo].first;
+        v[nodo].second--;
+        v[nodo].first = 0;
+        return ant;
+    }
+    izq = SrcDel(p, nodo * 2);
+    if (izq > 0)
+    {
+        v[nodo].first -= izq;
+        v[nodo].second--;
+        return izq;
+    }
+    else
+    {
+        der = SrcDel(p + vOr[nodo * 2] - v[nodo * 2].second, nodo * 2 + 1);
+        if (der > 0)
+        {
+            v[nodo].first -= der;
+            v[nodo].second--;
+        }
+        return der;
+    }
+}
+
+int main() {
+    long long n, pot = 1, i, q, p, k, aum;
+    cin >> n >> k;
+    q = n;
+    while (pot < n)
+        pot *= 2;
+    v.resize(pot * 2, { 0,0 });
+    vOr.resize(pot * 2, 1);
+    I.resize(pot * 2);
+    D.resize(pot * 2);
+    for (i = pot; i < pot + n; i++)
+    {
+        v[i].first=i-pot+1;
+        v[i].second = 1;
+    }
+    for (i = pot; i < I.size(); i++)
+    {
+        I[i] = i;
+        D[i] = i;
+    }
+    for (i = pot - 1; i > 0; i--)
+    {
+        I[i] = I[i * 2];
+        D[i] = D[i * 2 + 1];
+        v[i].first = v[i * 2].first + v[i * 2 + 1].first;
+        v[i].second = v[i * 2].second + v[i * 2 + 1].second;
+        vOr[i] = v[i].second;
+    }
+    aum = k%q;
+    while (q--)
+    {
+        cout << SrcDel(aum+ pot, 1) <<' ';
+        if(q==0)
+            break;
+        aum = (aum+k)%q;
+    }
+    return 0;
+}

521c2b517b94cc191ffe91e38848138f0830ae06
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-07-19 11:43:12 -0300

    Actualizar 2023

---
+++ OIAJ/Jurisdiccional/2023/Nivel 1/dividiendo.cpp
@@ -0,0 +1,15 @@
+//Link: https://juez.oia.unsam.edu.ar/task/145
+//Score: 100/100
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dividiendo(int numero, int veces)
+{
+    while(veces--)
+    numero/=2;
+    return numero;
+}
+

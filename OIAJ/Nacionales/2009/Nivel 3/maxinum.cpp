f23bc687e50a2c140c0692f5c1981d3e87f0a1cc
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-08-01 20:56:38 -0300

    Update maxinum.cpp

--- OIAJ/Nacionales/2009/Nivel 3/maxinum.cpp
+++ OIAJ/Nacionales/2009/Nivel 3/maxinum.cpp
@@ -1,36 +1,39 @@
 //Link: https://juez.oia.unsam.edu.ar/task/79
 //Score: 100/100
 #include <bits/stdc++.h>
+
 using namespace std;
-pair<long long,long long> cal(vector<long long>v)
+pair<long long, long long> cal (vector<long long> & v)
 {
     long long i, n=v.size();
-    vector<pair<long long, long long>>dp(n+1,{0,0});
-    for(i=0; i<n; i++)
+    vector<pair<long long, long long>>dp(n+1);
+    dp[0]={0,0};
+    for(i=0;i<n; i++)
     {
         dp[i+1]={dp[i].first,dp[i].second+v[i]};
         if(i>0)
         {
             dp[i+1]=max(dp[i+1],{dp[i-1].first+v[i]*v[i-1],dp[i-1].second});
-            //cout << dp[i+1].first << ' ' << dp[i+1].second << '\n';
         }
     }
     return dp[n];
 }
-int main() {
-    long long n, i, x, mul, sum;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     ifstream cin ("maxinum.in");
     ofstream cout ("maxinum.out");
+    long long n, i, a, mul, sum;
+    vector<long long>v,v2;
     cin >> n;
-    vector<long long>v, v2;
-
     for(i=0; i<n; i++)
     {
-        cin >> x;
+        cin >> a;
         if(i%2==0)
-            v.push_back(x);
+            v.push_back(a);
         else
-           v2.push_back(x);
+            v2.push_back(a);
     }
     pair<long long, long long>p=cal(v);
     mul=p.first;

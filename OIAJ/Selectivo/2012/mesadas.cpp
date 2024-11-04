b16307c574f48efbcac6a8d42fb4a6a7f9998e3e
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-11-03 21:08:27 -0300

    Create mesadas.cpp

---
+++ OIAJ/Selectivo/2012/mesadas.cpp
@@ -0,0 +1,38 @@
+//Link: https://juez.oia.unsam.edu.ar/task/84
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ifstream cin ("mesadas.in");
+    ofstream cout ("mesadas.out");
+    int n, i;
+    long long a, b;
+    cin >> n;
+    vector<pair<long long, long long>>v(n);
+    for(i=0; i<n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        if(v[i].first<v[i].second)
+            swap(v[i].first,v[i].second);
+        v[i].first*=-1ll;
+        v[i].second*=-1ll;
+    }
+    sort(v.begin(),v.end());
+    vector<long long>s;
+    for(i=0; i<n; i++)
+    {
+        auto it=lower_bound(s.begin(),s.end(),-v[i].second);
+        if(it==s.end())
+        {
+            s.push_back(-v[i].second);
+        }
+        else
+        {
+            *it=(-v[i].second);
+        }
+    }
+    cout << s.size();
+    return 0;
+}

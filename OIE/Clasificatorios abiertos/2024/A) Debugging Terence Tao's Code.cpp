87a0b3daf7a1bde8eef34ed775b8f0e753c89680
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-11-20 09:32:52 -0300

    Actualizar 2024

---
+++ OIE/Clasificatorios abiertos/2024/A) Debugging Terence Tao's Code.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll n=1e8, s=1, t=1, i, q;
+    cin >> q;
+    set<pair<ll,ll>>se;
+    while(s<n&&t<n)
+    {
+        
+        s+=t;
+        t+=s;
+        se.insert({t,s});
+        
+    }
+
+
+    while(q--)
+    {
+        cin >> i;
+        auto it=se.lower_bound(make_pair(i,0));
+        if(it->second>=i)
+        {
+            cout <<"OK\n";
+        }
+        else
+        {
+            cout << it->second << '\n';
+        }
+    }
+
+    return 0;
+}

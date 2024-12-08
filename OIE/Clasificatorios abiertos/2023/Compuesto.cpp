7fefb19ec08702648988dbecde8bc83eda11edaf
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-12-08 14:36:53 -0300

    Create Compuesto.cpp

---
+++ OIE/Clasificatorios abiertos/2023/Compuesto.cpp
@@ -0,0 +1,16 @@
+//Link: https://codeforces.com/gym/104597/problem/C
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long i, n, x;
+    cin >> n;
+    for(i=1; i<=n; i++)
+    {
+        x=i*2+1;
+        cout << x << ' ';
+    }
+    return 0;
+}

66787a1d7e53f45f5f4b0ca403f7c5d5c812c2af
NFaggi <161176274+NFaggi@users.noreply.github.com>
2024-12-28 17:04:54 -0300

    Update Bit Strings.cpp

--- CSES/Introductory Problems/Bit Strings.cpp
+++ CSES/Introductory Problems/Bit Strings.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
-const int MOD = 1e9+7;
-
+#define ll long long
 using namespace std;
-
-long long binpow(long long a, long long b)
+const int MOD=1e9+7;
+ll binpow(ll a, ll b)
 {
-    if (b == 1) return a;
-    if (b == 0) return 1;
-    long long tmp = binpow(a, b / 2);
-    tmp = (tmp * tmp) % MOD;
-    if (b % 2 == 1) tmp = (tmp * a) % MOD;
-    return tmp;
+    if(b==1)
+        return a;
+    if(b==0)
+        return 1;
+    ll ans;
+    ans=binpow(a,b/2)%MOD;
+    ans=(ans*ans)%MOD;
+    if(b%2!=0)
+        ans=(ans*a)%MOD;
+    return ans;
 }
 
 int main()
 {
-    long long n;
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    ll n;
     cin >> n;
-    cout << binpow(2,n)%MOD;
+    cout << binpow(2,n);
     return 0;
 }

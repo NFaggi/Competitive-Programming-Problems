ed851280b691996843a6889aad6052ba1f3ad305
NFaggi <161176274+NFaggi@users.noreply.github.com>
2025-01-06 13:34:28 -0300

    Actualizar Codeforces Round 993 (Div. 4)

---
+++ Codeforces/Codeforces Round 993 (Div. 4)/A) Easy Problem.cpp
@@ -0,0 +1,19 @@
+//Link: https://codeforces.com/contest/2044/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,cant=0;
+    cin >> n;
+    cout << n-1 <<'\n';
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}

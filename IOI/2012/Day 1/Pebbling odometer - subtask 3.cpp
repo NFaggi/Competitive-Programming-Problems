f4c780f88a30ce521d569253dd00a51a83c84b4b
NFaggi <161176274+NFaggi@users.noreply.github.com>
2025-02-02 15:28:30 -0300

    Create Pebbling odometer - subtask 3.cpp

---
+++ IOI/2012/Day 1/Pebbling odometer - subtask 3.cpp
@@ -0,0 +1,43 @@
+//Score: 19/19
+right
+a:
+pebble b
+move
+jump a
+b:
+move
+pebble fin
+put
+move
+pebble fin
+jump c
+c:
+pebble d
+move
+jump c
+d:
+left
+left
+move
+pebble fin2
+put
+move
+jump e
+e:
+pebble f
+move
+jump e
+
+f:
+right
+right
+jump b
+
+
+fin:
+left
+left
+move
+halt
+fin2:
+halt

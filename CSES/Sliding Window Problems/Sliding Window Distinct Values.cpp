9f97a238b3c6bfa6ea4a0a8420acbdbb1d6df044
NFaggi <161176274+NFaggi@users.noreply.github.com>
2025-05-19 22:49:02 -0300

    Create Sliding Window Distinct Values.cpp

---
+++ CSES/Sliding Window Problems/Sliding Window Distinct Values.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) int(x.size())
+#define forn(i,n) for(i=0; i<n; i++)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define se second
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n, k, i;
+    cin >> n >> k;
+    vector<ll>v(n);
+    for(i=0; i<n; i++)
+        cin >> v[i];
+    map<ll,ll>m;
+    set<ll>s;
+    for(i=0; i<k; i++)
+    {
+        s.insert(v[i]);
+        m[v[i]]++;
+    }
+    cout << sz(s) << ' ';
+    for(i=k; i<n; i++)
+    {
+        m[v[i-k]]--;
+        if(m[v[i-k]]==0)
+        {
+            auto it=s.find(v[i-k]);
+            if(it!=s.end())
+                s.erase(it);
+        }
+        s.insert(v[i]);
+        m[v[i]]++;
+        cout << sz(s) << ' ';
+    }
+    return 0;
+}

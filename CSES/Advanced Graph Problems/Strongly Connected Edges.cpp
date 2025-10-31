1e6c0f232d55b10ba801bc59b2c40a2e5cd3f595
NFaggi <161176274+NFaggi@users.noreply.github.com>
2025-10-31 00:10:01 -0300

    Refactor Strongly Connected Edges algorithm

--- CSES/Advanced Graph Problems/Strongly Connected Edges.cpp
+++ CSES/Advanced Graph Problems/Strongly Connected Edges.cpp
@@ -1,84 +1,90 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) int(x.size())
+#define forn(i,n) for(i=0; i<n; i++)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define se second
 using namespace std;
+
 const int MAXN=1e5+1;
-vector<int> grafo[MAXN];
-int vis[MAXN], low[MAXN], PA[MAXN], vi[MAXN], sig[MAXN];
-int act;
- 
-void DFS(int nod, int pad) {
-    int hijos = 0;
-    vis[nod] = low[nod] = ++act;
- 
-    for (int k : grafo[nod]) {
-        if (vis[k] == 0) {
-            hijos++;
-            DFS(k, nod);
-            low[nod] = min(low[nod], low[k]);
- 
-            if ((pad == 0 && hijos > 1) || (pad != 0 && low[k] >= vis[nod])) {
-                PA[nod] = 1;
-            }
-        } else if (k != pad) {
-            low[nod] = min(low[nod], vis[k]);
-        }
-    }
-}
-set<pair<int,int>>s;
-void DFSA(int nod, int pad)
+const int MAXM=2e5+1;
+
+vector<pair<ll,ll>>grafo[MAXN];
+vector<ll> gf[MAXN], ginv[MAXN];
+ll comp[MAXN], act=1;
+bool used[MAXM], vis[MAXN];
+
+void dfs(ll nod)
 {
-    int a,b;
-    for(int i=sig[nod]; i<int(grafo[nod].size()); i++)
+    for(auto k:grafo[nod])
     {
-        sig[nod]++;
-        a=nod;
-        b=grafo[nod][i];
-        if(a>b)
-            swap(a,b);
-        auto it=s.find({a,b});
-        if(it==s.end())
-        {
-            s.insert({a,b});
-            cout << nod << ' ' << grafo[nod][i] << '\n';
-            if(vi[grafo[nod][i]]==0&&grafo[nod][i]!=pad)
-            {
-                DFSA(grafo[nod][i],nod);
-            }
-        }
+        if(used[k.se])
+            continue;
+        used[k.se]=1;
+        gf[nod].pb(k.fr);
+        ginv[k.fr].pb(nod);
+        dfs(k.fr);
     }
-    vi[nod]=1;
 }
-int main() {
+vector<ll>topo;
+
+void dfsT(ll nod)
+{
+    vis[nod]=1;
+    for(auto k:gf[nod])
+    {
+        if(vis[k])
+            continue;
+        dfsT(k);
+    }
+    topo.pb(nod);
+}
+
+void dfsC(ll nod)
+{
+    comp[nod]=act;
+    for(auto k:ginv[nod])
+    {
+        if(comp[k])
+            continue;
+        dfsC(k);
+    }
+}
+
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, m, a, b, div=0;
+    ll n, m, i, a, b;
     cin >> n >> m;
-    act = 0;
-    for(int i=0; i<m; i++)
+    for(i=0; i<m; i++)
     {
         cin >> a >> b;
-        grafo[a].push_back(b);
-        grafo[b].push_back(a);
+        grafo[a].pb({b,i});
+        grafo[b].pb({a,i});
     }
-    for (int i = 1; i <= n; ++i) {
-        if (vis[i] == 0) {
-            if(div==1)
-            {
-                cout << "IMPOSSIBLE";
-                return 0;
-            }
-            div=1;
-            DFS(i, 0);
-        }
-    }
- 
-    for(int i=1; i<=n; i++)
+    dfs(1);
+    dfsT(1);
+    dfsC(topo.back());
+
+    for(i=1; i<=n; i++)
     {
-        if(PA[i])
+        if(comp[i]!=act)
         {
             cout << "IMPOSSIBLE";
             return 0;
         }
     }
-    DFSA(1,0);
+    for(i=1; i<=n; i++)
+    {
+        for(auto k:gf[i])
+        {
+            cout << i << ' ' << k << '\n';
+        }
+    }
     return 0;
 }

5026f60eac4292c984723f5c5238d8fbf5669d06
NFaggi <161176274+NFaggi@users.noreply.github.com>
2025-12-24 20:31:36 -0300

    Update Finding Periods.cpp

--- CSES/String Algorithms/Finding Periods.cpp
+++ CSES/String Algorithms/Finding Periods.cpp
@@ -1,41 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
-#define all(x) x.begin(),x.end()
-#define pb push_back 
 #define sz(x) int(x.size())
+#define forn(i,n) for(i=0; i<n; i++)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define se second
 using namespace std;
- 
+
+const int MAXN=1e6+1;
+
+ll z[MAXN];
+
+void calcZ(string a)
+{
+    ll l=0, r=0, k;
+    for(ll i=1; i<sz(a); i++)
+    {
+        k=i-l;
+        if(i<=r)
+            z[i]=min(r-i+1,z[k]);
+        while(i+z[i]<sz(a)&&a[z[i]]==a[i+z[i]])
+            z[i]++;
+        if(i+z[i]-1>r)
+        {
+            l=i;
+            r=i+z[i]-1;
+        }
+    }
+}
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     string s;
     cin >> s;
-    vector<ll>lps(sz(s),0);
-    ll len=0, i=1;
-    while(i<sz(s))
-    {
-        if(s[i]==s[len])
-        {
-            len++;
-            lps[i]=len;
-            i++;
-        }
-        else
-        {
-            if(len>0)
-                len=lps[len-1];
-            else
-                i++;    
-        }
-    }
-    vector<ll>ans;
-    
-    while(len)
-    {
-        ans.pb(sz(s)-len);
-        len=lps[len-1];
-    }
-    ans.pb(sz(s));
-    for(auto k:ans)
-        cout << k << ' ';
+    calcZ(s);
+    ll n=sz(s);
+    for(ll i=0; i<n; i++)
+        if(z[i]==n-i)
+             cout << n-z[i] << ' ';
+    cout << n;
     return 0;
 }
